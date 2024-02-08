0d6dd0be91b33042fcba16198358038ce91265e0
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2024-02-08 21:42:34 +0100

    constness

--- traktor/src/calculate.cpp
+++ traktor/src/calculate.cpp
@@ -7,7 +7,6 @@
 CoordPoint project_screen_point_to_coord(const int x_point_screen, const int y_point_screen, const int x_half, const int y_flucht) 
 {
     CoordPoint p;
-    //p.x = std::abs(x_point_screen - x_half);
     p.x = x_point_screen - x_half;
     p.y = y_flucht + y_point_screen;
     return p;
@@ -41,7 +40,7 @@ int project_x_inbetween_first_row(const int x_baseline, const unsigned int refli
     return x;
 }
 
-int delta_from_nearest_refline(int x, const unsigned int refline_distance)
+int delta_from_nearest_refline(const int x, const unsigned int refline_distance)
 {
     int half_refline = refline_distance / 2;
 
