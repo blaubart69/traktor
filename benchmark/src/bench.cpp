6486fa2c15af67aacab775e577d207591c471dd9
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2024-02-22 00:00:08 +0100

    added vector16 benchmark

--- benchmark/src/bench.cpp
+++ benchmark/src/bench.cpp
@@ -4,6 +4,15 @@
 #include "calc_classic/calc_classic.h"
 #include "calc_baseline/calc_baseline.h"
 
+void print_result(const char* impl_name, std::chrono::_V2::system_clock::rep duration_ns,
+    size_t points, int32_t delta_pixels_sum, size_t in_range, size_t out_range)
+{
+    printf("%20s: %11.3f ms, points: %12lu, delta_pixels_sum: %10d, in: %lu, out: %lu\n", 
+        impl_name
+        , ( (float)duration_ns / 1000000 )
+        , points, delta_pixels_sum, in_range, out_range );
+}
+
 void run_classic(float *delta_pixels_sum, size_t *in_range, size_t *out_range, size_t *points)
 {
     const int screen_width = 960;
@@ -43,7 +52,7 @@ void run_classic(float *delta_pixels_sum, size_t *in_range, size_t *out_range, s
         }
     }
 }
-void bench_classic(size_t frames)
+void bench_classic(const char* impl_name,size_t frames)
 {
     auto start = std::chrono::high_resolution_clock::now();
 
@@ -60,10 +69,7 @@ void bench_classic(size_t frames)
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>( end - start ).count();
 
-    printf("CLASSIC           : %11.3f ms, points: %lu, delta_pixels_sum: %5.1f, in: %lu, out: %lu\n", 
-        ( (float)duration / 1000000 )
-        , points, delta_pixels_sum, in_range, out_range );
-
+    print_result(impl_name, duration, points, delta_pixels_sum, in_range, out_range);
 }
 
 //typedef bool (*pfBaselineImpl)(int,int, const CalcSettings&, int*);
@@ -122,25 +128,86 @@ void bench_a_baseline(const char* impl_name, size_t frames, pfImpl baselineImpl)
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>( end - start ).count();
 
-    printf("%s: %11.3f ms, points: %lu, delta_pixels_sum: %5d, in: %lu, out: %lu\n", 
-        impl_name
-        , ( (float)duration / 1000000 )
-        , points, delta_pixels_sum, in_range, out_range );
+    print_result(impl_name, duration, points, delta_pixels_sum, in_range, out_range);
 }
 
+void bench_v16(const char* impl_name, size_t frames)
+{
+    const int screen_width = 960;
+    const int screen_height = 720;
+
+    CalcSettingsShort calcSettings(
+          screen_width / 2
+        , screen_height
+        , 0     // refSettings.rowPerspectivePx
+        , 160   // refSettings.rowSpacingPx
+        , 0     // refSettings.offset
+        , 2     // row_count
+    );
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+        int delta_pixels_sum = 0;
+        size_t in_range = 0;
+        size_t out_range = 0;
+        size_t points = 0;
+
+        bool ok[ 16 ];
+        int16_t v_x[ 16 ];
+        int16_t v_y[ 16 ];
+
+        uint8_t v_idx = 0;
+
+        for ( size_t i=0; i < frames; i++) 
+        {
+            for ( int16_t x=0; x<screen_width;x++) 
+            {
+                for ( int16_t y=0; y<screen_height;y++) 
+                {
+                    if ( v_idx < 16 )
+                    {
+                        v_x[v_idx] = x;
+                        v_y[v_idx] = y;
+                        v_idx++;
+                    }
+                    if ( v_idx == 16 )
+                    {
+                        v_idx = 0;
+
+                        int delta_pixels;
+                        calc_delta_from_nearest_refline_16(v_x, v_y, calcSettings, &delta_pixels, ok );
+                        delta_pixels_sum += delta_pixels;
+
+                        for ( int z=0; z < 16; z++)
+                        {
+                            if ( ok[ z ] )  { in_range++; }
+                            else            { out_range++; }
+                        }
+                        points += 16;
+                    }
+                }
+            }
+        }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>( end - start ).count();
+
+    print_result(impl_name, duration, points, delta_pixels_sum, in_range, out_range);
+}
 
 int main(int argc, char* argv[])
 {
-    size_t frames = 30 * 10;
+    size_t frames = 1;
 
     for ( int i=1; i <= 1; i++)
     {
         printf("--- %d. ---\n", i);
-        bench_classic(frames);
-        bench_a_baseline<CalcSettings>     ("baseline float    ", frames, calc_baseline_delta_from_nearest_refline);
+        bench_classic                      ("Classic",            frames);
+        bench_a_baseline<CalcSettings>     ("baseline float",     frames, calc_baseline_delta_from_nearest_refline);
         bench_a_baseline<CalcSettings>     ("baseline int (mul)", frames, calc_baseline_delta_from_nearest_refline_int);
         bench_a_baseline<CalcSettings>     ("baseline float mul", frames, calc_baseline_delta_from_nearest_refline_float_mul);
         bench_a_baseline<CalcSettingsShort>("baseline short int", frames, calc_baseline_delta_from_nearest_refline_short_int);
+        bench_v16                          ("baseline v16",       frames);
     }
 
     return 0;
---
+++ benchmark/src/calc_baseline/calc_baseline_v16.cpp
@@ -0,0 +1,134 @@
+#include <cmath>
+
+#include "calc_baseline.h"
+
+struct CoordPoint {
+    int16_t x;
+    int16_t y;
+};
+
+#define PRIVATE static
+
+/*
+struct CalcSettings
+{
+    const short int x_half;
+    const short int rowPerspectivePx;
+    const short int y_baseline;
+    const short int offset;
+    const unsigned short int refline_distance;
+    const unsigned short int half_refline_distance;
+    const unsigned short int range_baseline;
+
+    CalcSettings(short int x_half, short int y_screen_size, short int rowPerspectivePx, short int refline_distance, short int offset, short int row_count) 
+    :   x_half(x_half)
+    ,   rowPerspectivePx(rowPerspectivePx)
+    ,   y_baseline(rowPerspectivePx + y_screen_size)
+    ,   offset(offset)
+    ,   refline_distance(refline_distance)
+    ,   half_refline_distance(refline_distance / 2)
+    ,   range_baseline( ( refline_distance * row_count ) + (refline_distance / 2) )
+    {}
+};
+*/
+//
+// 0,0 ... fluchtpunkt
+//
+PRIVATE void project_screen_point_to_coord_16(const short int * __restrict x_point_screen, const short int * __restrict y_point_screen, CoordPoint * __restrict point, const short int x_half, const short int rowPerspectivePx) 
+{
+    for ( int i = 0; i < 16; i++ ) {
+        point[ i ].x = x_point_screen[ i ] - x_half;
+        point[ i ].y = rowPerspectivePx + y_point_screen[ i ];
+    }
+}
+//
+//  Fluchtpunkt: y=0
+//  baseline:    line on the lower end of the screen
+//  y_baseline:  vertical distance from Fluchtpunkt to lower end of the screen.
+//               screen height + y_fluchtpunkt
+//
+PRIVATE void project_x_onto_baseline_16(const CoordPoint * __restrict point, const short int y_baseline, int32_t * __restrict x_base_line, bool * __restrict ok) 
+{
+    for ( int i = 0; i < 16; i++ ) {
+        x_base_line[ i ] = ( y_baseline * point[ i ].x ) / (float)point[ i ].y;
+    }
+}
+
+PRIVATE void apply_offset_16(int32_t * __restrict x, short int offset, int32_t * __restrict x_offset)
+{
+    for ( int i = 0; i < 16; i++ ) {
+        x_offset[ i ] =  x[ i ] - offset;
+    }
+}
+
+PRIVATE void is_within_range_16(int32_t * __restrict x, const int16_t range_baseline, bool * __restrict ok)
+{
+    for ( int i = 0; i < 16; i++ ) {
+        //ok[ i ] = ((unsigned short int)std::abs(x[ i ]) < range_baseline ) ? false : ok[ i ];
+        ok[ i ] =  std::abs(x[ i ]) < range_baseline ;
+    }
+}
+
+PRIVATE void project_x_inbetween_first_row_16(const int32_t * __restrict x_baseline, const int16_t refline_distance, int16_t * __restrict x_baseline_first_row) 
+{
+    if ( refline_distance == 0)
+    {
+        // 2024-02-10 Spindler
+        // don't know if this makes sense.
+        // refline_distance == 0 means there are no reflines?
+        for ( int i = 0; i < 16; i++ ) {
+            x_baseline_first_row[ i ] = x_baseline[ i ];
+        }
+    }
+    else
+    {
+        for ( int i = 0; i < 16; i++ ) {
+            x_baseline_first_row[ i ] = x_baseline[ i ] % refline_distance;
+        }
+    }
+}
+
+PRIVATE void distance_to_nearest_refline_on_baseline(const short int * __restrict x_first_row, const short int refline_distance, const short int half_refline_distance, short int * __restrict delta_pixels)
+{
+    for ( int i = 0; i < 16; i++ ) {
+        if ( x_first_row[ i ] >=  half_refline_distance ) {
+           delta_pixels[ i ] = x_first_row[ i ] - refline_distance;
+        } else if ( x_first_row[ i ] <= -half_refline_distance ) {
+           delta_pixels[ i ] = x_first_row[ i ] + refline_distance;
+        } else {
+           delta_pixels[ i ] = x_first_row[ i ];
+        }
+    }
+}
+
+void calc_delta_from_nearest_refline_16(const short int * __restrict x_screen, const short int * __restrict y_screen, const CalcSettingsShort& settings, int32_t *my_pixels, bool * __restrict ok)
+{
+    CoordPoint point[ 16 ];
+    //bool ok[ 16 ] = { true, };
+
+    project_screen_point_to_coord_16(x_screen,y_screen, point, settings.x_half, settings.rowPerspectivePx);
+
+    int32_t x_baseline[ 16 ];
+    project_x_onto_baseline_16(point, settings.y_baseline, x_baseline, ok );
+
+    int32_t x_baseline_offset[ 16 ];
+    apply_offset_16(x_baseline, settings.offset, x_baseline_offset);
+
+    is_within_range_16(x_baseline_offset, settings.range_baseline, ok );
+
+    short int x_baseline_first_row[ 16 ];
+    project_x_inbetween_first_row_16(x_baseline_offset, settings.refline_distance,x_baseline_first_row);
+
+    short int delta_pixels[ 16 ];
+    distance_to_nearest_refline_on_baseline(x_baseline_first_row, settings.refline_distance, settings.half_refline_distance, delta_pixels);
+
+    *my_pixels = 0;
+    for ( int i = 0; i < 16; i++ ) {
+        if ( ok[i] )
+        {
+            *my_pixels += delta_pixels[ i ];
+        }
+    }
+
+    //*my_ok+=16;
+}
