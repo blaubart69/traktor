ff2c347a9d668dce3875d5041912ae9c01695a05
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2024-07-01 06:24:57 +0200

    also the non simd functions are now storing the points in an array before starting the loop. As it is in real life. :-)

--- benchmark/src/bench.cpp
+++ benchmark/src/bench.cpp
@@ -74,6 +74,7 @@ void bench_classic(const char* impl_name,size_t frames, int rows)
 
 //typedef bool (*pfBaselineImpl)(int,int, const CalcSettings&, int*);
 
+/*
 template<typename S, typename pfImpl>
 void run_baseline_impl(
     int screen_width, int screen_height,
@@ -98,7 +99,8 @@ void run_baseline_impl(
         }
     }
 }
-template<typename tySettings, typename pfImpl>
+*/
+template<typename tySettings, typename T, typename pfImpl>
 void bench_a_baseline(const char* impl_name, size_t frames, int rows, pfImpl baselineImpl)
 {
     const int screen_width = 960;
@@ -120,9 +122,46 @@ void bench_a_baseline(const char* impl_name, size_t frames, int rows, pfImpl bas
         size_t out_range = 0;
         size_t points = 0;
 
+        #define N 64
+
+        T v_x[ N ] __attribute__ ((aligned (128)));
+        T v_y[ N ] __attribute__ ((aligned (128)));
+
+        uint8_t v_idx = 0;
+
         for ( size_t i=0; i < frames; i++) 
         {
-            run_baseline_impl(screen_width, screen_height, &delta_pixels_sum, &in_range, &out_range, &points, calcSettings, baselineImpl);
+            //run_baseline_impl(screen_width, screen_height, &delta_pixels_sum, &in_range, &out_range, &points, calcSettings, baselineImpl);
+            for ( int x=0; x<screen_width;x++) 
+            {
+                for ( int y=0; y<screen_height;y++) 
+                {
+                    if ( v_idx < N )
+                    {
+                        v_x[v_idx] = x;
+                        v_y[v_idx] = y;
+                        v_idx++;
+                    }
+                    if ( v_idx == N )
+                    {
+                        v_idx = 0;
+
+                        for ( int v=0; v<N; v++) {
+                            int delta_pixels;
+                            if ( ! baselineImpl(v_x[v], v_y[v], calcSettings, &delta_pixels) )
+                            {
+                                out_range += 1;
+                            }
+                            else
+                            {
+                                in_range += 1;
+                                delta_pixels_sum += delta_pixels;
+                            }
+                            points += 1;
+                        }
+                    }
+                }
+            }
         }
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -130,7 +169,7 @@ void bench_a_baseline(const char* impl_name, size_t frames, int rows, pfImpl bas
 
     print_result(impl_name, duration, points, delta_pixels_sum, in_range, out_range);
 }
-
+/*
 template<typename tySettings, typename pfImpl>
 void bench_a_baseline_loop(const char* impl_name, size_t frames, int rows, pfImpl baselineImpl)
 {
@@ -160,7 +199,7 @@ void bench_a_baseline_loop(const char* impl_name, size_t frames, int rows, pfImp
 
     print_result(impl_name, duration, points, delta_pixels_sum, in_range, out_range);
 }
-
+*/
 
 void bench_v16(const char* impl_name, size_t frames, int rows)
 {
@@ -308,20 +347,20 @@ int main()
     for ( int i=1; i <= 1; i++)
     {
         printf("--- %d. rows: %d, frames: %lu---\n", i, rows, frames);
-        bench_classic                      ("Classic",               frames, rows);
-        bench_a_baseline<CalcSettings>     ("baseline float (simple)", frames, rows, calc_baseline_delta_from_nearest_refline_simple);
-        bench_a_baseline<CalcSettings>     ("baseline int (mul)",    frames, rows, calc_baseline_delta_from_nearest_refline_int);
-        bench_a_baseline<CalcSettings>     ("baseline float mul",    frames, rows, calc_baseline_delta_from_nearest_refline_float_mul);
-        bench_a_baseline<CalcSettingsShort>("baseline short int",    frames, rows, calc_baseline_delta_from_nearest_refline_short_int);
-        bench_a_baseline<CalcSettingsFloat>("baseline only float",   frames, rows, calc_baseline_delta_from_nearest_refline_only_float);
-        bench_v16                          ("baseline v16",          frames, rows);
-        bench_highway<int32_t>             ("highway int32",         frames, rows, deltapx::run_hwy_calc_delta_pixels_int32);
-        bench_highway<int32_t>             ("highway int32 only",    frames, rows, deltapx::run_hwy_calc_delta_pixels_int32_only);
-        bench_highway<int16_t>             ("highway int16",         frames, rows, deltapx::run_hwy_calc_delta_pixels_int16);
-        bench_highway<int16_t>             ("highway int16 fdiv",    frames, rows, deltapx::run_hwy_calc_delta_pixels_int16_fdiv);
-        bench_highway<int16_t>             ("highway int16 fp16",    frames, rows, deltapx::run_hwy_calc_delta_pixels_fp16);
+        bench_classic                               ("Classic",               frames, rows);
+        bench_a_baseline<CalcSettings,int32_t>      ("baseline float (simple)", frames, rows, calc_baseline_delta_from_nearest_refline_simple);
+        bench_a_baseline<CalcSettings,int32_t>      ("baseline int (mul)",    frames, rows, calc_baseline_delta_from_nearest_refline_int);
+        bench_a_baseline<CalcSettings,int32_t>      ("baseline float mul",    frames, rows, calc_baseline_delta_from_nearest_refline_float_mul);
+        bench_a_baseline<CalcSettingsShort,int16_t> ("baseline short int",    frames, rows, calc_baseline_delta_from_nearest_refline_short_int);
+        bench_a_baseline<CalcSettingsFloat,int32_t> ("baseline only float",   frames, rows, calc_baseline_delta_from_nearest_refline_only_float);
+        bench_v16                                   ("baseline v16",          frames, rows);
+        bench_highway<int32_t>                      ("highway int32",         frames, rows, deltapx::run_hwy_calc_delta_pixels_int32);
+        bench_highway<int32_t>                      ("highway int32 only",    frames, rows, deltapx::run_hwy_calc_delta_pixels_int32_only);
+        bench_highway<int16_t>                      ("highway int16",         frames, rows, deltapx::run_hwy_calc_delta_pixels_int16);
+        bench_highway<int16_t>                      ("highway int16 fdiv",    frames, rows, deltapx::run_hwy_calc_delta_pixels_int16_fdiv);
+        bench_highway<int16_t>                      ("highway int16 fp16",    frames, rows, deltapx::run_hwy_calc_delta_pixels_fp16);
         
-        bench_a_baseline_loop<CalcSettings>("baseline simple loop",  frames, rows, calc_baseline_full_loop);
+        //bench_a_baseline_loop<CalcSettings>("baseline simple loop",  frames, rows, calc_baseline_full_loop);
     }
     printf("=====\n");
     deltapx::print_target();
